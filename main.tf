b0e95b5cd2ecdcfe9c3d4977fe4368705590b07a
Chinonsowasky25 <84383267+Chinonsowasky25@users.noreply.github.com>
2024-09-04 07:07:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,1 @@
-provider "google" {
-  project = sonic-totem-416019
-  region  = us-central1
-}
 
-resource "google_container_cluster" "primary" {
-  name               = "my-cluster"
-  location           = var.region
-  initial_node_count = 2
-  min_master_version = "1.22"
-  node_config {
-    machine_type = "e2-medium"
-  }
-}
-
-# Add more resources as needed
